cc285501069985b76c42aa4d000dfd1a02053d0a
George Ceja <Net.george.sky@gmail.com>
2021-10-09 17:39:56 -0700

    add JS to form

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 console.log("JS File Loaded")
 
 AOS.init();
+
+const formDiv = document.querySelector('.form-input');
+const formSubmitMessage = document.getElementById('submit-message');
+const show = "block";
+const hide = "none";
+
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
+
+document.getElementById('form-submit-button').addEventListener("click", function(button){
+  formDiv.style.display = hide;
+  formSubmitMessage.style.display = show;
+
+  setTimeout(function() {
+    formDiv.style.display = show;
+    formSubmitMessage.style.display = hide;
+  }, 5000);
+  }
+);
